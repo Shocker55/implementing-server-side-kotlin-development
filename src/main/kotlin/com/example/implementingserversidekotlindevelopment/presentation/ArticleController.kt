6434fdd93b6c6c4886d4ab027c1a4f3b9684b549
Shocker55 <bialetti360@gmail.com>
2023-12-14 17:17:58 +0900

    feat: ユースケース(異常系)の作成
    ShowArticleUseCaseErrorExceptionという関数を作成し、例外をユースケースの左側(Eitherのエラーの方)に詰め直すラッパーの役割を行う。
    (fold関数、ArrowライブラリのEitherを使っている)
    明示的に例外を発生させることで、@ExceptionHandler と onShowArticleUseCaseErrorException によって例外処理が実行される。

--- src/main/kotlin/com/example/implementingserversidekotlindevelopment/presentation/ArticleController.kt
+++ src/main/kotlin/com/example/implementingserversidekotlindevelopment/presentation/ArticleController.kt
@@ -3,10 +3,13 @@ package com.example.implementingserversidekotlindevelopment.presentation
 // 下記の package は OpenAPI Generator によって生成された package
 import com.example.implementingserversidekotlindevelopment.openapi.generated.controller.ArticlesApi
 import com.example.implementingserversidekotlindevelopment.openapi.generated.model.Article
+import com.example.implementingserversidekotlindevelopment.openapi.generated.model.GenericErrorModel
+import com.example.implementingserversidekotlindevelopment.openapi.generated.model.GenericErrorModelErrors
 import com.example.implementingserversidekotlindevelopment.openapi.generated.model.SingleArticleResponse
 import com.example.implementingserversidekotlindevelopment.usecase.ShowArticleUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -21,7 +24,7 @@ class ArticleController(val showArticleUseCase: ShowArticleUseCase) : ArticlesAp
          * 作成済記事の取得
          */
         val createdArticle = showArticleUseCase.execute(slug).fold(
-            { throw TODO() }, // エラーハンドリングは後で実装
+            { throw ShowArticleUseCaseErrorException(it) },
             { it }
         )
 
@@ -38,3 +41,45 @@ class ArticleController(val showArticleUseCase: ShowArticleUseCase) : ArticlesAp
         )
     }
 }
+
+/**
+ * 単一記事取得ユースケースがエラーを戻した時の Exception
+ *
+ * このクラスの例外が発生した時に、@ExceptionHandler で例外処理を行う
+ *
+ * @property error
+ */
+data class ShowArticleUseCaseErrorException(val error: ShowArticleUseCase.Error) : Exception()
+
+/**
+ * ShowArticleUseCaseErrorException をハンドリングする関数
+ *
+ * ShowArticleUseCase.Error の型に合わせてレスポンスを分岐させる
+ */
+@ExceptionHandler(value = [ShowArticleUseCaseErrorException::class])
+fun onShowArticleUseCaseErrorException(e: ShowArticleUseCaseErrorException): ResponseEntity<GenericErrorModel> =
+    when (val error = e.error) {
+        /**
+         * 原因: slug に該当する記事が見つからなかった
+         */
+        is ShowArticleUseCase.Error.NotFoundArticleBySlug -> ResponseEntity<GenericErrorModel>(
+            GenericErrorModel(
+                errors = GenericErrorModelErrors(
+                    body = listOf("${error.slug.value} に該当する記事は見つかりませんでした")
+                )
+            ),
+            HttpStatus.NOT_FOUND
+        )
+
+        /**
+         * 原因: バリデーションエラー
+         */
+        is ShowArticleUseCase.Error.ValidationErrors -> ResponseEntity<GenericErrorModel>(
+            GenericErrorModel(
+                errors = GenericErrorModelErrors(
+                    body = error.errors.map { it.message }
+                )
+            ),
+            HttpStatus.FORBIDDEN
+        )
+    }
